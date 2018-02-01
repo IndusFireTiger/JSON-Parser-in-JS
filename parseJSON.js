c75af936b6c23933468cf2c66c07ef8ab5f215c0
Sindhu <indusfiretiger@gmail.com>
2018-02-01 08:54:38 +0530

    added parseObject()

--- parseJSON.js
+++ parseJSON.js
@@ -4,15 +4,23 @@ var fs = require('fs')
       console.log('error', err);
     }
     console.log('STRING here: \n', str);
-    let result = parseArray(str)
-    console.log('FINAL RESULT: ', result);
+    let result = parseObject(str)
+    console.log('\nFINAL RESULT: ', result);
   });
 
 function parseNull(data){
+  let spaceTrimedStr = parseSpace(data)
+  if(spaceTrimedStr != null){// if no space found then pass string directly
+    data = spaceTrimedStr[1]
+  }
   return (data.startsWith('null'))? [null, data.slice(4)] : null
 }
 
 function parseBool(data){
+  let spaceTrimedStr = parseSpace(data)
+  if(spaceTrimedStr != null){// if no space found then pass string directly
+    data = spaceTrimedStr[1]
+  }
   if(data.startsWith('true'))
     return [true, data.slice(4)]
   else if(data.startsWith('false'))
@@ -22,6 +30,10 @@ function parseBool(data){
 }
 
 function parseNumber(data) {
+  let spaceTrimedStr = parseSpace(data)
+  if(spaceTrimedStr != null){// if no space found then pass string directly
+    data = spaceTrimedStr[1]
+  }
   var regEx = /[\+\-]?\d*\.?\d+(?:[Ee][\+\-]?\d+)?/
   var match = data.match(regEx)
   if (match != null && match[0] != -1) {
@@ -33,6 +45,10 @@ function parseNumber(data) {
 }
 
 function parseString(data) {
+  let spaceTrimedStr = parseSpace(data)
+  if(spaceTrimedStr != null){// if no space found then pass string directly
+    data = spaceTrimedStr[1]
+  }
   if(!data.startsWith('"')) return null
   var btw_quotes = /"[^"]*"/
   var match = data.match(btw_quotes)
@@ -49,10 +65,19 @@ function parseSpace(data) {
 
 
 function parseComma(data) {
+  let spaceTrimedStr = parseSpace(data)
+  if(spaceTrimedStr != null){// if no space found then pass string directly
+    data = spaceTrimedStr[1]
+  }
   return (data.startsWith(','))? [',',data.slice(1)]:null
 }
 
 function parseColon(data) {
+  // console.log('parseColon data:', data);
+  let spaceTrimedStr = parseSpace(data)
+  if(spaceTrimedStr != null){// if no space found then pass string directly
+    data = spaceTrimedStr[1]
+  }
   return (data.startsWith(':'))? [':',data.slice(1)]:null
 }
 
@@ -63,20 +88,14 @@ function parseArray(data) {
     while(!data.startsWith(']')){
       // get one element at a time
       let element = parseValue(data)
-      if(element == null)
-        break     // if no/no more element found the end here
-      else
-        array.push(element[0])  // if element found then add to a resultant array
+      if(element == null) break     // if no/no more element found the end here
+      else array.push(element[0])  // if element found then add to a resultant array
 
       // remove comma btw the elements
       let commaTrimedString = parseComma(element[1])
-      if(commaTrimedString == null){
-        data = element[1]
-      }else{
-        data = commaTrimedString[1]
-      }
+      if(commaTrimedString == null) data = element[1]
+      else data = commaTrimedString[1]
     }
-
   }
   console.log('FINAL ARRAY: \n', array);
   return [array, data.slice(1)]
@@ -94,4 +113,34 @@ function parseValue(data){
   if(extractedElement == null) extractedElement = parseNumber(data) // check for number element
 
   return extractedElement
+}
+
+function parseObject(data){
+  console.log('parseObj - data:', data)
+  if(data.startsWith('{')){
+    var obj = {}, keyPlusRest, valuePlusRest, key, value, colonTrimedRest, moreProperty
+    data = data.slice(1)
+    while(!data.startsWith(']')){
+
+      keyPlusRest = parseValue(data) // extract key
+      if(keyPlusRest == null) break
+      else key = keyPlusRest[0]
+      console.log('\nkey', key);
+
+      colonTrimedRest = parseColon(keyPlusRest[1]) // remove colon
+      if(colonTrimedRest == null) break
+      else valuePlusRest = parseValue(colonTrimedRest[1]) // extract value
+      if(valuePlusRest == null) break
+      else value = valuePlusRest[0]
+      console.log('value', value);
+
+      obj[key] = value // add key-value pair to object
+      console.log('obj', obj);
+
+      moreProperty = parseComma(valuePlusRest[1]) //look for more such pairs
+      if(moreProperty == null) break
+      else data = moreProperty[1]
+    }
+  }
+  return [obj, valuePlusRest[1]]
 }
